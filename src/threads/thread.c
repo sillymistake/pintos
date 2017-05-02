5e095cb0f38a08f08ea1bda56bf1841018152f62
sillymistake <1074369959@qq.com>
2017-05-02 18:15:11 +0800

    5.2

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -217,6 +217,10 @@ thread_create (const char *name, int priority,
   /* Add to run queue. */
   thread_unblock (t);
 
+  if(thread_current()->priority < priority)
+  {
+    thread_yield();
+  }
   return tid;
 }
 
@@ -352,6 +356,7 @@ void
 thread_set_priority (int new_priority) 
 {
   thread_current ()->priority = new_priority;
+  thread_yield();
 }
 
 /* Returns the current thread's priority. */
